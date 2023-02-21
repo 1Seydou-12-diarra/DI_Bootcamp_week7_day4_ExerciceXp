d5a778138ddfb6c29d48f57a3f3156a46618859e
Seydou Diarrassouba <115084625+1Seydou-12-diarra@users.noreply.github.com>
2023-02-21 09:45:04 +0100

    update

--- Exercice2/header.php
+++ Exercice2/header.php
@@ -1,9 +1,13 @@
 <!-- header.php -->
-<html>
-<head>
-  <title>Mon en-tête</title>
-</head>
-<body>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="public/css/style.css">
+        <title>PHP include Example</title>
+    </head>
+    <body>
   <header>
     <?php include 'navbar.php'; ?>
   </header>
