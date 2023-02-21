22df8e24107433a34707e05e63c6094c367b8d1c
Seydou Diarrassouba <115084625+1Seydou-12-diarra@users.noreply.github.com>
2023-02-21 09:41:06 +0100

    update

--- exercice1/index.php
+++ exercice1/index.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
 
-<h1>BIENVENUE SUR MA PAGE ADACI</h1>
 
-<?php
-// appel des differents fichiers  dans le fichier indexin
-include ('header.php');
+<?php include ('header.php');?>
 
-include ('footer.php');
-?>
-</body>
-</html>
+<main>
+  <h1>BIENVENUE SUR MA PAGE ADACI</h1>
+</main>
+
+<?php include ('footer.php');?>
