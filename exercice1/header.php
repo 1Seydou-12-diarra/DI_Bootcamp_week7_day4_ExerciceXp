c38c45ead181eace9ec51a41ba1003b75ae0ffb6
Seydou Diarrassouba <115084625+1Seydou-12-diarra@users.noreply.github.com>
2023-02-21 09:34:31 +0100

    update

--- exercice1/header.php
+++ exercice1/header.php
@@ -1,3 +1,12 @@
-<header>
-    <h1 style ="color:green"> Diarrassouba</h1>
-</header>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="public/css/style.css">
+        <title>PHP include Example</title>
+    </head>
+    <body>
+        <header>
+            <h1 style ="color:green"> Diarrassouba</h1>
+       </header>
